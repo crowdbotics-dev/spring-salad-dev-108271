3bc7f32f0b618cb6add48e4befad867b4e25d71c
Crowdbotics <engineering@crowdbotics.com>
2023-08-08 23:19:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-salad-dev-108271",
+  url: "https://spring-salad-dev-108271.botics.co",
+  api: "https://spring-salad-dev-108271.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
